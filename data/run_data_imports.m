2ec0cce98d1a98a528f3cc2bb0d2dc524a7021aa
brendan-busch <brendan.busch@uwa.edu.au>
2022-10-24 13:42:44 +0800

    Updated dataset.

--- data/run_data_imports.m
+++ data/run_data_imports.m
@@ -5,3 +5,7 @@ import_wq_data;
 import_wq_data2;
 
 run_summaries;
+
+copyfile('Summary_v3/Berowra.shp','../gis/Berowra.shp');
+copyfile('Summary_v3/Berowra.dbf','../gis/Berowra.dbf');
+copyfile('Summary_v3/Berowra.shx','../gis/Berowra.shx');
