a5277e965c6c848cbf4d6c26e5e0e101c24a6c78
brendan-busch <brendan.busch@uwa.edu.au>
2022-10-24 20:29:05 +0800

    Data import and plotting

--- data/import_wq_data2.m
+++ data/import_wq_data2.m
@@ -8,23 +8,16 @@ nvars = sstr(:,2);
 units = sstr(:,4);
 
 
-[snum,sstr ] = xlsread('220126Filled_site_info.xlsx','A16:D10000');
-sites_all = sstr(:,1);
-inc = 1;
-for i = 1:length(sites_all)
-if ~isempty(sites_all{i})
-    sites(inc) = sites_all(i);
-    X(inc) = snum(i,1);
-    Y(inc) = snum(i,2);
-    agency(inc) = sstr(i,4);
-    inc = inc + 1;
-end
-end
+[snum,sstr ] = xlsread('220126Filled_site_info.xlsx','A2:D17');
+sites = snum(:,1);
+X = snum(:,2);
+Y = snum(:,3);
+agency = sstr(:,1);
 
 
 [~,headers] = xlsread('Formatted_Data2.xlsx','C1:AA1');
 
-[~,sitedata] = xlsread('Formatted_Data2.xlsx','A2:A10000');
+[sitedata,~] = xlsread('Formatted_Data2.xlsx','A2:A10000');
 [~,sDate] = xlsread('Formatted_Data2.xlsx','B2:B10000');
 mdate = datenum(sDate,'dd/mm/yyyy');
 [Data,~] = xlsread('Formatted_Data2.xlsx','C2:AA100000');
@@ -34,34 +27,33 @@ mdate = datenum(sDate,'dd/mm/yyyy');
 
 for i = 1:length(sites)
     
-    %sss = find(sitedata == sites(i));
+    sss = find(sitedata == sites(i));
     
-    sss = find(strcmpi(sitedata,sites{i})== 1);
-    
-    %sitename = ['S',num2str(sites(i))];
-    sitename = sites{i};
+    sitename = ['S',num2str(sites(i))];
     
     
-    for j = 1:length(headers)
-        
-        ttt = find(~isnan(Data(sss,j)) == 1);
-        
-        ggg = find(strcmpi(ovars,headers{j}) == 1);
-        
-        newvar = nvars{ggg(1)};
-        newconv = conv(ggg(1));
-        newunits = units{ggg};
-        if strcmpi(newvar,'Ignore') == 0
+    if ~isempty(sss)
+        for j = 1:length(headers)
             
-            Berowra.(sitename).(newvar).Data = Data(sss(ttt),j) * newconv;
-            Berowra.(sitename).(newvar).Date = mdate(sss(ttt),1);
-            Berowra.(sitename).(newvar).Depth(1:length(sss(ttt)),1) = 0;
-            Berowra.(sitename).(newvar).X = X(i);
-            Berowra.(sitename).(newvar).Y = Y(i);
-            Berowra.(sitename).(newvar).Name = sitename;
-            Berowra.(sitename).(newvar).Units = newunits;
-            Berowra.(sitename).(newvar).Agency = agency{i};
+            ttt = find(~isnan(Data(sss,j)) == 1);
             
+            ggg = find(strcmpi(ovars,headers{j}) == 1);
+            
+            newvar = nvars{ggg(1)};
+            newconv = conv(ggg(1));
+            newunits = units{ggg};
+            if strcmpi(newvar,'Ignore') == 0
+                
+                Berowra.(sitename).(newvar).Data = Data(sss(ttt),j) * newconv;
+                Berowra.(sitename).(newvar).Date = mdate(sss(ttt),1);
+                Berowra.(sitename).(newvar).Depth(1:length(sss(ttt)),1) = 0;
+                Berowra.(sitename).(newvar).X = X(i);
+                Berowra.(sitename).(newvar).Y = Y(i);
+                Berowra.(sitename).(newvar).Name = sitename;
+                Berowra.(sitename).(newvar).Units = newunits;
+                Berowra.(sitename).(newvar).Agency = agency{i};
+                
+            end
         end
     end
 end
--- plotting/polygon_timeseries_plotting/run_hchb.m
+++
@@ -1,6 +0,0 @@
-%plottfv_polygon Gen0
-%plottfv_polygon Gen1
-%plottfv_polygon Gen1_5
-%plottfv_polygon Gen1_5_ALS
-%plottfv_polygon Gen2
-plottfv_polygon Multi
